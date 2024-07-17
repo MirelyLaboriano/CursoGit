58e6ad25ac293a7b0ce1a57035450a6b973e3049
Mirely Laboriano <laborianocuevam@gmail.com>
2024-07-17 08:56:14 -0500

    agregar

---
+++ src/titulo.tsx
@@ -0,0 +1,11 @@
+function Titulo(){
+    //jsx
+    const nombre ="Chanchito Feliz";
+    if(nombre){
+      return <h1>hola {nombre}</h1>;
+    }
+    return <h1>Hola Mundo</h1>;
+  
+  }
+  
+  export default Titulo;
